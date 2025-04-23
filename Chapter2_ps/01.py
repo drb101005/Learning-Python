087676d98375660ce178f7137b6203b824bb6e57
drb101005 <drb101005@gmail.com>
2025-04-23 19:48:09 +0530

    A few codes

---
+++ Chapter2_ps/01.py
@@ -0,0 +1,7 @@
+#a program to add 2 numbers
+
+a = int(input("Enter the number 1 :  "))
+
+b = int(input("Enter the number 2 :  "))
+
+print(a+b)
---
+++ Chapter2_ps/02.py
@@ -0,0 +1,7 @@
+# A code to find remainder when a number is divided by Z
+
+a = 345
+
+b = 7
+
+print("Remainder when a is divided by b is ", a % b)
