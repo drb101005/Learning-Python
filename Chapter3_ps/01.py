7f6139cfc95b41ce26de1c608a8e209b00f74001
drb101005 <drb101005@gmail.com>
2025-04-23 20:06:26 +0530

    ..

---
+++ Chapter3_ps/01.py
@@ -0,0 +1,5 @@
+# A Good morning text for a user
+
+name = input("Enter your name : ")
+
+print(f"Good Morning {name} Sir/ Madam")
---
+++ Chapter3_ps/02.py
@@ -0,0 +1,8 @@
+letter = ''''
+Dear name,
+You are selected !
+date
+
+'''
+
+print(letter.replace("name" , "Dhruv ").replace("date" , "10-10-2005"))
