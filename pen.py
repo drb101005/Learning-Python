8bb74c2b610a8a005a82581091fa74c84952f2cb
drb101005 <drb101005@gmail.com>
2025-05-19 16:12:55 +0530

    Update pen.py

--- pen.py
+++ pen.py
@@ -11,7 +11,7 @@ pen.pensize(5)
 for _ in range(4):
     pen.forward(100)  # Move forward by 100 units
     pen.right(90)     # Turn right by 90 degrees
-
+    pen.left(32)
 # Hide turtle and display the window
 pen.hideturtle()
 turtle.done()
