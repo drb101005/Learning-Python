6254d52ee6045d445d1edc9bf8501984448db665
drb101005 <drb101005@gmail.com>
2025-05-19 16:13:29 +0530

    Update pen.py

--- pen.py
+++ pen.py
@@ -8,7 +8,7 @@ pen.color("red")
 pen.pensize(5)
 
 # Draw a square
-for _ in range(8):
+for _ in range(800):
     pen.forward(100)  # Move forward by 100 units
     pen.right(90)     # Turn right by 90 degrees
     pen.left(32)
