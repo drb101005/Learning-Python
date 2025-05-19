0f271a643bcc4095758bb3cda0685efe06b8b9e2
drb101005 <drb101005@gmail.com>
2025-05-19 16:14:35 +0530

    Update pen.py

--- pen.py
+++ pen.py
@@ -8,10 +8,11 @@ pen.color("red")
 pen.pensize(5)
 
 # Draw a square
-for _ in range(800):
+for _ in range(8):
     pen.forward(100)  # Move forward by 100 units
     pen.right(90)     # Turn right by 90 degrees
     pen.left(32)
+    pen.fillcolor("blue")
 # Hide turtle and display the window
 pen.hideturtle()
 turtle.done()
