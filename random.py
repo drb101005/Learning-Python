32f4cd0c653b0e749eaca1af5ce7803d101fdef6
drb101005 <drb101005@gmail.com>
2025-05-20 18:51:24 +0530

    Update random.py

--- random.py
+++ random.py
@@ -3,13 +3,16 @@ import time
 # List of words to choose from
 words = ["apple", "banana", "cherry", "dragonfruit", "elephant", "flamingo", "grape", "honeydew"]
 
-# Function to generate a pseudo-random index using current time
+# Function to generate a pseudo-random word using current time
 def generate_random_word():
     current_time = time.time()
-    index = int(current_time) % len(words)
+    # Get fractional part of current time and multiply it to get more variability
+    fractional = current_time - int(current_time)
+    mixed_value = int(fractional * 1000000)  # Get microseconds
+    index = mixed_value % len(words)
     return words[index]
 
-
+# Run the generator
 if __name__ == "__main__":
- word = generate_random_word()
-  print("Random word:", word)
+    word = generate_random_word()
+    print("Random word:", word)
