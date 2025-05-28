a00fcda62c1f145c79607696c785a030e8b515f5
drb101005 <drb101005@gmail.com>
2025-05-28 18:53:24 +0530

    .

--- Chapter4_ps/01.py
+++ Chapter4_ps/01.py
@@ -1,1 +1,20 @@
-fruit1 = input("Enter the name of the first fruit")
+list1 = []
+fruit1 = str(input("Enter the name of the first fruit:"))
+fruit2 = str(input("Enter the name of the second fruit:"))
+fruit3 = str(input("Enter the name of the third fruit:"))
+fruit4 = str(input("Enter the name of the forth fruit:"))
+fruit5 = str(input("Enter the name of the fifth fruit:"))
+fruit6 = str(input("Enter the name of the sixth fruit:"))
+fruit7 = str(input("Enter the name of the seventh fruit:"))
+
+
+list1.append(fruit1)
+list1.append(fruit2)
+list1.append(fruit3)
+list1.append(fruit4)
+list1.append(fruit5)
+list1.append(fruit6)
+list1.append(fruit7)
+print(list1)
+
+'''Works!!!!'''
---
+++ Chapter4_ps/05.py
@@ -0,0 +1,6 @@
+'''write a program to find the number of zeros in the following tuple'''
+
+a = (7,0,8,0,0,9)
+b = a.count(0)
+
+print(b)
