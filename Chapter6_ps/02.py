d76d775e294cfb0123ba67c9b4c0f3f884e415e8
drb101005 <drb101005@gmail.com>
2025-06-02 19:06:07 +0530

    Update 02.py

--- Chapter6_ps/02.py
+++ Chapter6_ps/02.py
@@ -1,5 +1,15 @@
 '''The check if a student is passing or not
 conditions : 40 % all avg 
  33 % in each subject for 3 subjects
- 
-'''
+ '''
+
+mark1 = int(input("Enter the marks of subject 1: "))
+mark2 = int(input("Enter the marks of subject 2: "))
+mark3 = int(input("Enter the marks of subject 3: "))
+
+average = (mark1 + mark2 + mark3) / 3
+
+if average >= 40 and mark1 >= 33 and mark2 >= 33 and mark3 >= 33:
+    print("You passed!!!!")
+else:
+    print("You failed")
