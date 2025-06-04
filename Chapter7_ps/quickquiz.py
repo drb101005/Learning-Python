37eb3e7b81d92d9b7a46831d38c1f3af0746d4d4
drb101005 <drb101005@gmail.com>
2025-06-04 21:11:23 +0530

    Update quickquiz.py

--- Chapter7_ps/quickquiz.py
+++ Chapter7_ps/quickquiz.py
@@ -1,7 +1,4 @@
-# Prompt the user to enter a number
 i = int(input("Enter a number: "))
-
-# Continue looping until the number is 50 or more
 while i < 50:
-    i += 1  # Increment the number by 1
-    print(i)  # Print the updated number
+    i += 1
+    print(i)
