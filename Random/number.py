fe9348f90c528461cecc0940256946dceb8fe8c5
drb101005 <drb101005@gmail.com>
2025-06-05 22:28:13 +0530

    Create number.py

---
+++ Random/number.py
@@ -0,0 +1,17 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
+
+def factorial(n):
+    if n < 0:
+        return None
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n - 1)
+
+def number_sequence(n):
+    return list(range(1, n + 1))
