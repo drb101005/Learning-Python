6af5c3f1851722f691ebe6f3a33a650dd5ad02b5
drb101005 <drb101005@gmail.com>
2025-06-07 22:19:44 +0530

    Update 02.py

--- Chapter8_ps/PS/02.py
+++ Chapter8_ps/PS/02.py
@@ -1,11 +1,15 @@
 #write a program to convert Celsius to Fahrenheit
 
-cel = int(input("Enter the degree is Celsius : "))
-far = 0
-
 def converter():
+    cel = int(input("Enter the degree is Celsius : "))
+    far = 0
     far = (cel * 1.8) + 32
     print(far)
     return 0
 
-converter()
+#converter()
+
+#assuming i have to run the same thing for n times
+
+while(0<1):
+    converter()
