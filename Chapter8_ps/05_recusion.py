526c6bb66a846075bc3d8e2858766d6998a04dd4
drb101005 <drb101005@gmail.com>
2025-06-07 22:04:44 +0530

    Update 05_recusion.py

--- Chapter8_ps/05_recusion.py
+++ Chapter8_ps/05_recusion.py
@@ -1,11 +1,8 @@
-def factiorial(n):
-    if(n ==1 or n ==0):
+def factorial(n):
+    if n == 1 or n == 0:
         return 1
-    return n * factiorial(n-1)
+    return n * factorial(n - 1)
 
 n = int(input("Enter a number: "))
-
-f = factiorial(n)
-
+f = factorial(n)
 print(f)
-
