138b0c6269013bd86362019e051120ec798e90ea
drb101005 <drb101005@gmail.com>
2025-06-07 10:08:49 +0530

    Update 02.py

---
+++ Chapter7_ps/Practice codes/02.py
@@ -0,0 +1,7 @@
+#Write a program to great people whos name starts with D
+
+l = ["Dhruv", "Rahul", "Durva","Don","Yati","Dude"]
+
+for name in l:
+    if(name.startswith("D")):
+        print(f"Hello {name}")
