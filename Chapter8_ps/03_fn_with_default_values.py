40f2af38d7f9e601fbf79ac2fe89a45a6d7aefe4
drb101005 <drb101005@gmail.com>
2025-06-07 19:04:24 +0530

    .

---
+++ Chapter8_ps/03_fn_with_default_values.py
@@ -0,0 +1,8 @@
+def goodday(name, ending= " Thanks"):
+    print("Good Day, " + name + ending)
+
+
+goodday("Dhruv", " Thank you ")
+goodday("Daksh")
+
+#so if i give a value for ending then it will take that or else it will just take the default value
