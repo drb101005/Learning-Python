d2215c95bf4b9b1a663a3982ec87bedfcb7a1f57
drb101005 <drb101005@gmail.com>
2025-06-07 17:46:04 +0530

    .

---
+++ Chapter8_ps/01_functions.py
@@ -0,0 +1,15 @@
+#write a function to find area of n number of traingles
+
+def area():
+    B = float(input("Enter the Base Length: "))
+    H = float(input("Enter the Height: "))
+    A = B*H*0.5
+    print(f"The area is {A}")
+
+# area()    
+# area()    
+# area()    
+#For n number of times
+
+while(1>0):
+    area()
