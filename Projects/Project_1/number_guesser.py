097a7f9a7a42d5337ef9127cc4acb993c4840b6f
drb101005 <84022106+drb101005@users.noreply.github.com>
2025-06-12 20:27:29 +0530

    Update number_guesser.py

--- Projects/Project_1/number_guesser.py
+++ Projects/Project_1/number_guesser.py
@@ -6,6 +6,7 @@ i = 0
 number1 = random.choice[(0,1,2,3,4,5)]
 number2 = random.choice([0,1,2,3,4,5,6,7,8,9])
 number3 = rancomd.choice[(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)]
+diffucult_level = int(input("Enter the level of difficulty from level 1,2,3 :"))
 # The plan is to ask user What difficulty he  wants ,in number make 3 types first 0-5 then O to 9, then 0 to 15 
 # add an impossible difficulty where the numbers are from 0 to 9 but after every wrong guess the choice of number changes
 
@@ -23,7 +24,7 @@ def game1():
         
         return 0
 
-while True:
+while difficulty_level = 1:
     i = i + 1
     if game() == 0:
        
@@ -46,12 +47,34 @@ def game2():
         
         return 0
 
-while True:
+while difficulty_level = 2:
     i = i + 1
     if game() == 0:
        
         print(Style.BRIGHT + Fore.RED + f"It took you {i} times...")
         break
+
+def game3():
+
+    guess = int(input(Style.BRIGHT + Fore.MAGENTA + "Enter your guess (from 0 to 15) : "))
+    if(guess < number):
+        print("Low!")
+      
+    elif(number < guess):
+        print("High!")
+        
+    else:
+        print(Style.BRIGHT + Fore.BLUE + "You guessed the number right!!")
+        
+        return 0
+
+while difficulty_level = 3:
+    i = i + 1
+    if game() == 0:
+       
+        print(Style.BRIGHT + Fore.RED + f"It took you {i} times...")
+        break
+
 
 # ask the user if there are multiplayers
 #add difficulties
