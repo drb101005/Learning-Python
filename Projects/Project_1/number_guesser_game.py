b63a3fc9a0d1821051263fd37bf8cdfdf888aa27
drb101005 <84022106+drb101005@users.noreply.github.com>
2025-06-16 13:56:05 +0530

    Update number_guesser_game.py

--- Projects/Project_1/number_guesser_game.py
+++ Projects/Project_1/number_guesser_game.py
@@ -7,5 +7,17 @@
 leved plan 0-5 0-10. 0 to 15 in the last difficulty 0-1 but the random number changes after every wrong attempt
 
 level = int(input ("Enter the level of diffutly you want :))
+,
+def level1():
 
-def level1
+    guess = int(input(Style.BRIGHT + Fore.MAGENTA + "Enter your guess (from 0 to ) : "))
+    if(guess < number):
+        print("Low!")
+      
+    elif(number < guess):
+        print("High!")
+        
+    else:
+        print(Style.BRIGHT + Fore.BLUE + "You guessed the number right!!")
+        
+        return 0
