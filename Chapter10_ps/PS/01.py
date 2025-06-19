d12e319fae3556f0e551a762f4179a5a3fe59c05
drb101005 <drb101005@gmail.com>
2025-06-19 17:58:02 +0530

    Update 01.py

--- Chapter10_ps/PS/01.py
+++ Chapter10_ps/PS/01.py
@@ -1,2 +1,15 @@
 #Create a class "programmer" for storing info of programmers working in microsoft
 
+class programmer:
+    company = "Microsoft"
+    def __init__(self, name, salary, pin):
+        self.name = name
+        self.salary = salary
+        self.pin = pin
+
+
+p = programmer("Dhruv ",9000000, 401202)
+print(p.name, p.salary, p.pin, p.company)
+
+r = programmer("Rohan ",9000000, 401202)
+print(r.name, r.salary, r.pin, r.company)
