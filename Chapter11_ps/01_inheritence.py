616945e736c974af276cca5c1013f64c93c19bc4
drb101005 <drb101005@gmail.com>
2025-06-26 21:49:20 +0530

    Create 01_inheritence.py

---
+++ Chapter11_ps/01_inheritence.py
@@ -0,0 +1,25 @@
+class Employee:
+    company = "G-CORP"
+    def show(self):
+        print(f"The name of the Employee is {self.name} and the salary is {self.salary}")
+
+
+# class Programmer:
+#     company = "Cal-T"
+#     def show(self):
+#         print(f"The name is {self.name} and the salary is {self.salary}")
+
+#     def showLanguage(self):
+#         print(f"The name is {self.name} and he is good with {self.language} language")
+#so intead of writing the whole above part again you can just inherit from a parent/base class...
+
+class Programmer(Employee):
+    company = "CAL-T"
+    def showLanguage(self):
+        print(f"The name is {self.name} and he is good with {self.language} language")
+
+
+a = Employee()
+b = Programmer()
+
+print(a.company, b.company)
