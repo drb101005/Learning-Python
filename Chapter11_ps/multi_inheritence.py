b1fa1fc7db7d510b2e50c69602b883636d8fb84c
drb101005 <drb101005@gmail.com>
2025-06-26 21:58:18 +0530

    Update multi_inheritence.py

--- Chapter11_ps/multi_inheritence.py
+++ Chapter11_ps/multi_inheritence.py
@@ -10,8 +10,15 @@ class C:
     def method(self):
         print("C method")
 
-class D(A, B, C):
-    pass
+# class D(A, B, C):
+#     pass
 
+
+
+class D(A, B, C):
+    def method(self):
+        A.method(self)
+        B.method(self)
+        C.method(self)
 d = D()
 d.method()
