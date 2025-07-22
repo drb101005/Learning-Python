ce3e9f705b37f3f11f5a9637833d835e50da8d5d
drb101005 <drb101005@gmail.com>
2025-07-22 21:25:19 +0530

    Update percentage_done.py

--- Random/percentage_done.py
+++ Random/percentage_done.py
@@ -1,10 +1,25 @@
-no_of_vids = int(input("Enter the number of videos: "))
+no_of_vids = int(input("Enter the number of videos: "))   
+done = int(input("Enter the number of vids done so far: "))
+
 def counter():
-    done = int(input("Enter the number of vids done so far: "))
+    global done  
 
     percentage_completed = (done / no_of_vids) * 100
+    print(f"Progress: {percentage_completed:.2f}%")
 
-    print(percentage_completed)
+    change = input("Increase or decrease (i/d): ").lower()  
+    if change == 'i':
+        if done < no_of_vids:
+            done += 1
+        else:
+            print("All videos are already completed.")
+    elif change == 'd':
+        if done > 0:
+            done -= 1
+        else:
+            print("You can't go below 0 videos.")
+    else:
+        print("Invalid input. Use 'i' to increase or 'd' to decrease.")
 
 while True:
     counter()
